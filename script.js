8e590e1fe650495e2f61830231aa4569f564d1cf
esket1 <baykovdr23@st.ithub.ru>
2025-05-08 04:09:42 +0300

    cards

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-const cardNumberInput = document.getElementById("card-number")
+const cardNumberInput = document.getElementById("inputCardNumber")
 const submitButton = document.getElementById("submit")
 
-
+let cardNumberFormated = ""
 cardNumberInput.addEventListener("input", () => {
     let value = cardNumberInput.value.split("").filter(char => char >= "0" && char <= "9").join("")
-    
-    let cardNumberFormated = ""
-    
+    cardNumberFormated = ""
     for (let i = 0; i < value.length; i++) {
         if (i > 0 && i % 4 === 0) {
             cardNumberFormated += " "
@@ -17,22 +15,25 @@ cardNumberInput.addEventListener("input", () => {
     cardNumberInput.value = cardNumberFormated
 })
 
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault()
+    const bankInput = document.getElementById("inputBank")
+    const paymentSystemInput = document.getElementById("inputPaymentSystem")
+    const cardOwnerInput = document.getElementById("inputCardOwner")
+    const container = document.getElementById("container")
+    if(!bankInput.value || !paymentSystemInput.value || !cardOwnerInput.value) {
+        return alert("Заполните все поля!")
+    }
 
-// submitButton.addEventListener("click", () => {
-//     const bankInput = document.getElementById("bank")
-//     const paymentSystemInput = document.getElementById("payment-system")
-//     const cardOwnerInput = document.getElementById("card-owner")
-//     const container = document.getElementById("container")
-
-//     container.insertAdjacentHTML("beforeend", `
-//         <div id="card" class="${bank}-card">
-//             <p id="card-text">Credit Card</p>
-//             <img id="${bankInput.value}" src="cards/images/banks/${bankInput.value}.svg" alt="${bankInput.value}" width="80" height="30">
-//             <img id="chip" src="cards/images/chip.png" alt="" width="30" height="30">
-//             <p id="card-number">${formattedValue}</p>
-//             <p id="card-owner">${cardOwnerInput.value}</p>
-//             <p id="date">03/23</p>
-//             <img id="${paymentSystemInput.value}" src="cards/images/payment-systems/${paymentSystemInput.value}.svg" alt="${paymentSystemInput.value}" width="90" height="90">
-//         </div>
-//     `)
-// })
+    container.insertAdjacentHTML("beforeend", `
+        <div id="card" class="${bankInput.value}Card">
+            <p id="cardText">Credit Card</p>
+            <img id="${bankInput.value}" src="cards/images/banks/${bankInput.value}.svg" alt="${bankInput.value}" width="80" height="30">
+            <img id="chip" src="cards/images/chip.png" alt="" width="30" height="30">
+            <p id="cardNumber">${cardNumberFormated}</p>
+            <p id="cardOwner">${cardOwnerInput.value}</p>
+            <p id="date">03/23</p>
+            <img id="${paymentSystemInput.value}" src="cards/images/payment-systems/${paymentSystemInput.value}.svg" alt="${paymentSystemInput.value}" width="90" height="90">
+        </div>
+    `)
+})
